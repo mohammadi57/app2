1baf9bbc5730692d269bba23ddc7a448211f48ec
Your Name <you@example.com>
2020-06-08 12:13:25 +0430

    add int b

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         privat int a=1000;
+         int a=1000;
+         int b=124;
     }
 }
